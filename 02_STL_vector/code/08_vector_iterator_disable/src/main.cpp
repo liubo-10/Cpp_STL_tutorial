0f075e567683c2d67ebebe520c08a92c5f4eb2f6
liubo00 <liubo@zerozero.cn>
2025-04-07 21:09:29 +0800

    kldfgsgjkj

--- 02_STL_vector/code/08_vector_iterator_disable/src/main.cpp
+++ 02_STL_vector/code/08_vector_iterator_disable/src/main.cpp
@@ -33,19 +33,14 @@ int main()
     printf("--------------------begain-------------------\n");
     vector<int> v_test = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     auto it = v_test.begin();
+    v_test.reserve(100);
 
     cout << "size = " << v_test.size() << endl;
     cout << "capacity = " << v_test.capacity() << endl;
     cout << "addr of iter: " << &*it << endl
          << endl;
 
-    v_test.push_back(0); // 可能导致容量不足，触发内存重新分配
-    cout << "push_back 0: "<< endl;
-    cout << "size = " << v_test.size() << endl;
-    cout << "capacity = " << v_test.capacity() << endl;
-    cout << "addr of iter: " << &*it << endl
-         << endl;
-
+    v_test.reserve(100);
     // 将容量用完
     while (v_test.size() != v_test.capacity())
     {
