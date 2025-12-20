2a3904cb657d25cb3c2e79d8b669a162a9255870
liubo00 <liubo@zerozero.cn>
2025-12-20 08:00:36 +0800

    iksdjflsdlgjfdgf

--- 02_STL_vector/code/01_vector_create/src/01_vector_create.cpp
+++ 02_STL_vector/code/01_vector_create/src/01_vector_create.cpp
@@ -60,21 +60,30 @@ int main()
     // T m[] = {a, b, c, .....};
     // vector<T> vectorname(m, m + sizeof(m) / sizeof(T));
     int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> vector_5(a, a + 10); // 从数组区间[a[0], a[10])中获得初值,注意左开右闭，a[10]无意义这里助于理解
+    vector<int> vector_5(a, a + 10); // 从数组区间[a[0], a[10])中获得初值,注意左闭右开，a[10]无意义这里助于理解
     cout << "vector_5: ";
     print_vector(vector_5);
 
     // 6.将[v.begin(),v.end())区间中的元素拷贝给本身，注意左闭右开
     // vector<T> vectorname2(vectorname1.begin(), vectorname1.end());
-    vector<int> vector_6(vector_5.begin(), vector_5.end() - 5); // temp [temp.begin(),temp.end()-5)区间的元素赋给fifth，注意左开右闭
+    // temp [temp.begin(),temp.end()-5)区间的元素赋给fifth，注意左开右闭
+    vector<int> vector_6(vector_5.begin(), vector_5.end() - 5);
     cout << "vector_6: ";
     print_vector(vector_6);
+
+    vector<int> vectora6(vector_5.begin(), vector_5.end());
+    cout << "vectora6: ";
+    print_vector(vectora6);
 
     // 7.列表初始化构造容器
     // vector<T> vectorname{a, b, c, .....};
     vector<int> vector_7 = {-1, 0, 3, 5, 7, 9}; // 列表初始化
     cout << "vector_7: ";
     print_vector(vector_7);
+
+    vector<int> vectora7{1, 0, -3, -5, -7, -9}; // 列表初始化
+    cout << "vectora7: ";
+    print_vector(vectora7);
 
     printf("--------------------end----------------------\n");
     // cin.get();
@@ -86,3 +95,13 @@ int main()
 /*****************************************************************************
  * end of file
  ******************************************************************************/
+
+/* --------------------begain-------------------
+vector_1: 
+vector_2: 0 0 0 0 0 0 0 0 0 0 
+vector_3: 1 1 1 1 1 1 1 1 1 1 
+vector_4: 1 1 1 1 1 1 1 1 1 1 
+vector_5: 0 1 2 3 4 5 6 7 8 9 
+vector_6: 0 1 2 3 4 
+vector_7: -1 0 3 5 7 9 
+--------------------end---------------------- */
