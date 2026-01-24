ececaa00f61ff7ece2d75a84001e5ed009747fd0
liubo00 <liubo@zerozero.cn>
2026-01-24 18:38:41 +0800

    sfksdfjkj

--- 02_STL_vector/code/05_vector_insert_element/src/05_vector_insert_element.cpp
+++ 02_STL_vector/code/05_vector_insert_element/src/05_vector_insert_element.cpp
@@ -7,10 +7,6 @@
  * |    V2     2025-12-26
  * ---------------------------------------
  * | @brief   : vector 插入元素测试
- * 
- * 
- * 
- * https://blog.csdn.net/2401_88591507/article/details/146243185?ops_request_misc=&request_id=&biz_id=102&utm_term=vector%20insert&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-0-146243185.142^v102^pc_search_result_base8&spm=1018.2226.3001.4187
  ******************************************************************************/
 #include <stdio.h>  // C语言的标准库，包含C语言流操作 printf等
 #include <iostream> // 包含输入和输出操作
@@ -48,7 +44,7 @@ int main()
     cout << "在尾部插入3个10，打印 vector_11: ";
     print_vector(vector_11);
 
-    cout << "----------------------------------------" << endl;
+    cout << endl;
 
     //插入方式2: insert 函数
 
@@ -62,7 +58,7 @@ int main()
     print_vector(vector_21);
 
     vector<int> vector_22{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector_22.insert(vector_22.end(), 10); //在首位插入10
+    vector_22.insert(vector_22.end(), 10); //在末位插入10
     cout << "在末位插入1个10，打印 vector_22: ";
     print_vector(vector_22);
 
@@ -71,12 +67,12 @@ int main()
     //在迭代器指向的位置 pos 前面插入 count 个元素 value
 
     vector<int> vector_31{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector_31.insert(vector_31.begin(), 3, 10); //在首位插入10
+    vector_31.insert(vector_31.begin(), 3, 10); //在首位插入3个10
     cout << "在首位插入3个10，打印 vector_31: ";
     print_vector(vector_31);
 
     vector<int> vector_32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector_32.insert(vector_32.begin() + 3, 30); //在索引3的位置前面插入30
+    vector_32.insert(vector_32.begin() + 3, 3, 30); //在索引3的位置前面插入3个30
     cout << "在索引3的位置前面插入30，打印 vector_32: ";
     print_vector(vector_32);
 
@@ -84,17 +80,21 @@ int main()
     //template <class InputIterator>
     //void insert(const_iterator pos, InputIterator first, InputIterator last);
 
-    vector<int> vector_40 = {99, 100, 101};
-    vector<int> vector_41 = {1, 2, 3, 4, 5};
+    vector<int> vector_40{99, 100, 101};
+    vector<int> vector_41{1, 2, 3, 4, 5};
     vector_41.insert(vector_41.begin() + 2, vector_40.begin(), vector_40.end()); // 在索引2的位置插入 vector_40 的所有元素
     cout << "在索引2的位置插入 vector_40 的所有元素，打印 vector_41:";
-    print_vector(vector_32);
+    print_vector(vector_41);
 
     //5.插入初始化列表中的元素（C++11 及以上）
     //void insert(const_iterator pos, initializer_list<T> ilist);
 
     vector<int> vector_51 = {1, 2, 3, 4, 5};
     vector_51.insert(vector_51.begin() + 3, {99, 100, 101}); // 在索引3的位置插入初始化列表中的元素
+    cout << "在索引3的位置插入列表的所有元素，打印 vector_51:";
+    print_vector(vector_51);
+
+
 
     printf("--------------------end----------------------\n");
 
